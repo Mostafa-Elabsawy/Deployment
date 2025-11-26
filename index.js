68f215eb32cba3677f92f110b3912500d97f1eeb
Mostafa-Elabsawy <mostfaelabsawy@gmail.com>
2025-11-26 20:44:09 +0200

    edited

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import * as Routes from "./src/Routes.js";
 import { connectDB } from "./DB/connection.js";
+import cors from "cors";
 connectDB();
 const app = express();
-const prot = 4444;
+const port = process.env.PORT || 4444;
 app.use(express.json());
+app.use(cors());
 app.use("/users", Routes.userRouter);
-app.listen(prot, () => {
+app.listen(port, () => {
   console.log(`server started at http://localhost:${prot}`);
 });
